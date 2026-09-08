ac7fa42a773e3661455c8b2c4c0dc1d5648fe06f
JinKyo-max <jjg0413@gmail.com>
2026-09-08 23:09:40 +0900

    Add SeichiPlaceVo import

--- src/main/java/com/seichigo/mapper/AnimeWorkMapper.java
+++ src/main/java/com/seichigo/mapper/AnimeWorkMapper.java
@@ -6,22 +6,22 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 import com.seichigo.domain.AnimeWorkVo;
+import com.seichigo.domain.SeichiPlaceVo;
 
 @Mapper
 public interface AnimeWorkMapper {
 
-	//관리자가 아니메 등록하는 매퍼
-	 @Insert("INSERT INTO anime_work (title, description, thumbnail, release_year) " +
-	            "VALUES (#{title}, #{description}, #{thumbnail}, #{release_year})")
-	    void insertAnimeWork(AnimeWorkVo vo);
-	 
-	 @Select("SELECT * FROM anime_work ORDER BY work_id asc")
-	    List<AnimeWorkVo> selectAllAnimeWorks();
-	 
-	 @Select("SELECT * FROM anime_work WHERE work_id = #{id}")
-	 AnimeWorkVo selectWorkById(int id);
+    // 관리자가 아니메 등록하는 매퍼
+    @Insert("INSERT INTO anime_work (title, description, thumbnail, release_year) " +
+            "VALUES (#{title}, #{description}, #{thumbnail}, #{release_year})")
+    void insertAnimeWork(AnimeWorkVo vo);
 
-	 @Select("SELECT * FROM seichi_place WHERE work_id = #{workId}")
-	 List<SeichiPlaceVo> selectPlacesByWorkId(int workId);
+    @Select("SELECT * FROM anime_work ORDER BY work_id asc")
+    List<AnimeWorkVo> selectAllAnimeWorks();
 
+    @Select("SELECT * FROM anime_work WHERE work_id = #{id}")
+    AnimeWorkVo selectWorkById(int id);
+
+    @Select("SELECT * FROM seichi_place WHERE work_id = #{workId}")
+    List<SeichiPlaceVo> selectPlacesByWorkId(int workId);
 }
